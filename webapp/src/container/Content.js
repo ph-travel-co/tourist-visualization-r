7131cfe42b83e590df56cf269a422b6d1b31e20b
Bart@PTC <book.cebu@gmail.com>
2019-11-16 00:49:31 +0000

    :wq
    cleanup
    
     Please enter the commit message for your changes. Lines starting

--- webapp/src/container/Content.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import Container from "@material-ui/core/Container";
-
-const Content = props => {
-  return <Container maxWidth="md">{props.children}</Container>;
-};
-
-export default Content;
--- webapp/src/container/Cover.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import Box from "@material-ui/core/Box";
-
-const Cover = props => {
-  return <Box>{props.children}</Box>;
-};
-
-export default Cover;
